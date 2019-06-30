9cb602f99808ef624ed89805ecc1f7176a5e1af9
rafalszy <rafalszypulewski@gmail.com>
2019-06-30 18:15:15 +0200

    Add third test case+some fixes

---
+++ src/main/java/pageobjects/CareerFormPage.java
@@ -0,0 +1,14 @@
+package pageobjects;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class ContactPage extends BasePage {
+
+    public ContactPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+}
